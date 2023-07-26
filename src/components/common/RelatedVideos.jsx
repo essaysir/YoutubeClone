6c6730a336cc4b9f56f269771cb24fe38985780f
Sonjusun <sonjs7554@naver.com>
2023-07-26 15:46:14 +0900

    2023-07-26 유튜브 클론코딩 완료 및 배포 테스트

--- src/components/common/RelatedVideos.jsx
+++ src/components/common/RelatedVideos.jsx
@@ -6,14 +6,16 @@ import VideoCard  from './VideoCard';
 export default function RelatedVideos({id,name}) {
     const {youtube} = useYoutubeApi();
     const { isLoading ,error , data:videos} = useQuery( ['related', id] , ()=>
-        youtube.releatedVideos(id)); 
+        youtube.releatedVideos(id) ,
+        {staleTime: 1000*60*5} , 
+        ); 
     return (
         <>
             {isLoading && <p>Loading</p>}
             {error && <p>Something is Wrong</p>}
             {videos && <ul className=''>
                 {videos.map((video)=> (
-                    <VideoCard key={video.id} video={video}/>
+                    <VideoCard key={video.id} video={video} type='list'/> 
                 ))}
                 </ul>}       
         </>
--- src/components/common/VideoCard.jsx
+++ src/components/common/VideoCard.jsx
@@ -4,7 +4,7 @@ import { Navigate, useNavigate } from 'react-router-dom';
 
 
 
-export default function VideoCard({video}) {
+export default function VideoCard({video , type}) {
     const {title,  publishedAt , thumbnails, channelTitle} = video.snippet ;
     // 특정 url 로 이동해야하는 STEP 
     // 1. useNavigate() 정의  2. navigate('url') 실행 
@@ -12,10 +12,11 @@ export default function VideoCard({video}) {
     const handleClick = ()=>{
         navigate(`/videos/watch/${video.id}` , {state: {video}});
     };
+    const isList = type === 'list';
     return (
-        <li>
-            <img className='w-full cursor-pointer' onClick={handleClick} src={thumbnails.medium.url} alt={title} />
-            <div>
+        <li className={isList ? 'flex gap-1 m-2' : ''}>
+            <img className={isList ? 'w-60':'w-full'} onClick={handleClick} src={thumbnails.medium.url} alt={title} />
+            <div className={isList ? 'w-60 mr-2':'w-full'}>
                 <p className='font-semibold my-2 line-clamp-2'>{title}</p>
                 <p className='text-sm opacity-80'>{channelTitle}</p>
                 <p className='text-sm opacity-80'>{formatAgo(publishedAt,'ko')}</p>
